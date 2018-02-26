8dddbefe56ae716b4886aec9a5a719f6bb4ac3c6
patrikejane <sskmal@gmail.com>
2018-02-27 04:01:56 +0530

    fetch api to fetch charater data

--- VueApp#2/src/App.vue
+++ VueApp#2/src/App.vue
@@ -1,6 +1,7 @@
 <template lang="html">
   <div id="app" class="">
     <h3>{{title}}</h3>
+    <button type="button" class="btn btn-primary" name="button" @click="fetchCharacter">getCharacter</button>
   </div>
 </template>
 
@@ -10,6 +11,15 @@ export default {
   data(){
     return {
       title:'Genarate team'
+    }
+  },
+  methods:{
+    fetchCharacter(){
+      fetch('https://swapi.co/api/people/1',{
+        method:'GET',
+
+      }).then(response=> response.json())
+      .then(json=>console.log(json))
     }
   }
 }
